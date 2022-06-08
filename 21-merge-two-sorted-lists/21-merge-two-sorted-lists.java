6efb80ea20fa48f0b6542bc615b1d3ae601b1a17
Mohammad Ibrahim <103769738+Mohammad-ibrah1m@users.noreply.github.com>
2022-06-08 22:33:28 +0300

    Time: 1 ms (66.92%), Space: 43.7 MB (8.00%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -15,7 +15,7 @@ class Solution {
         
         while(l1 != null && l2 != null){
             
-            if(l1.val < l2.val){
+            if(l1.val <= l2.val){
                 
                 current.next = l1;
                 l1 = l1.next;
