1794dadc2c7211d054d0a55f563f93b6dd80664d
Mohammad Ibrahim <103769738+Mohammad-ibrah1m@users.noreply.github.com>
2022-06-08 12:07:55 +0300

    Time: 1 ms (65.29%), Space: 43.9 MB (58.25%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -10,7 +10,6 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-
         if(head == null){
             return null;
         }
@@ -18,13 +17,13 @@ class Solution {
             ListNode current = head;
             while(current.next != null){
                 if(current.val == current.next.val){
-                current.next = current.next.next;
+                    current.next = current.next.next;
+                }
+                else{
+                    current = current.next;
+                }
             }
-            else{
-                current = current.next;
-            }
-            }
-        return head;
+            return head;
         }
     }
 }
